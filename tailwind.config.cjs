7011e49bb319b1e758e73842ca2e94515e2dad81
tanmui1992 <tanmui1992@gmail.com>
2023-01-16 09:44:45 +0700

    feat: initial files

---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+      },
+      colors: {
+        primary: colors.red,
+        secondary: colors.yellow,
+        neutral: colors.gray,
+      },
+    },
+  },
+  plugins: [],
+};
