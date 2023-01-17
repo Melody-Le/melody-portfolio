5f323edf98addec0ceb6256a172945d808d30dfe
tanmui1992 <tanmui1992@gmail.com>
2023-01-17 13:52:37 +0700

    Feat: add techProjects array

--- src/containers/tech-projects/techProjectsList.js
+++ src/containers/tech-projects/techProjectsList.js
@@ -1,4 +1,4 @@
-export const techSkills = [
+export const techProjects = [
   {
     projectName: "Spoon Town",
     demo: "https://melody-le.github.io/spoon-town-app/filterPage.html",
@@ -10,27 +10,31 @@ export const techSkills = [
   {
     projectName: "aTom",
     demo: "https://atom-freelancer-app.onrender.com/idea-market",
-    code: "https://github.com/Melody-Le/spoon-town-app",
-    techStacks: "OOP | JS | HTML | CSS | Bootstrap| API | Figma ",
+    code: "https://github.com/Melody-Le/aTom",
+    techStacks:
+      "MongoDB | Express | NodeJS | EJS | Bootstrap | ImageKit.io | Figma ",
     description:
-      "A foodie app, User can find a place to eat, and read/post reviews about that restaurant. Data of restaurant fetch from Yelp API, Leaflet API (for map location)",
+      "aTom is Full-stack Freelancer app, allows people to view professional portfolios and find some great inspiration design for their dream home, Besides, they can post their job at aTom platform to find the right interior design for their own. On the other hand, designers can post their work/idea, to attract clients hiring them.",
   },
   {
-    projectName: "Spoon Town",
-    demo: "https://melody-le.github.io/spoon-town-app/filterPage.html",
-    code: "https://github.com/Melody-Le/spoon-town-app",
-    techStacks: "OOP | JS | HTML | CSS | Bootstrap| API | Figma ",
-    description:
-      "A foodie app, User can find a place to eat, and read/post reviews about that restaurant. Data of restaurant fetch from Yelp API, Leaflet API (for map location)",
+    projectName: "HelloWorld",
+    demo: "https://hello-world-fe.netlify.app/",
+    code: "https://github.com/will-j-c/react-hello-world",
+    techStacks:
+      "MongoDB | imageKit.io | Multer | NodeJS | ReactJS| React Router | MUI | SASS | JSON Web Token | GSAP | ZenHub | SendGrid API",
+    description: `With a mindset of constantly exploring new ideas and technologies, my team ( group of 3 developers) decided to build a community for developers to publish their products, find contributors to make amazing things happen. 
+      ◦	Implement code review & PR commits across the team
+      ◦	Practice project management tool with Zen hub
+      ◦	Quickly catch up on new knowledge from MUI, React
+      `,
   },
   {
-    projectName: "Spoon Town",
-    demo: "https://melody-le.github.io/spoon-town-app/filterPage.html",
-    code: "https://github.com/Melody-Le/spoon-town-app",
-    techStacks: "OOP | JS | HTML | CSS | Bootstrap| API | Figma ",
+    projectName: "Mikea",
+    demo: "https://mikea.netlify.app/",
+    code: "https://github.com/Melody-Le/Mikea-FE",
+    techStacks:
+      "SQL - PostgreSQL | NodeJS | ReactJS| React Router | MUI | JSON Web Token | Typescript | Sequelize ORM ",
     description:
-      "A foodie app, User can find a place to eat, and read/post reviews about that restaurant. Data of restaurant fetch from Yelp API, Leaflet API (for map location)",
+      "Mikea is an e-commerce web shop, allows user view and buy furniture, add product into cart, checkout, and can trace purchase history. With a big love Ikea product, I used data from Ikea products for this website, seed and store data by sequelize ORM.",
   },
 ];
-export const designSkills = [];
-export default { techSkills, designSkills };
