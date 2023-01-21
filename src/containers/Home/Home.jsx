364cd0d0553f5a50d0744739d750252eb2eaf885
tanmui1992 <tanmui1992@gmail.com>
2023-01-21 22:38:49 +0700

    feat: adjust anmation

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -18,7 +18,7 @@ function Home() {
         <Social />
         <div
           data-aos="fade-right"
-          className=" transition duration-150 ease-in-out w-9 basis-1/2 md:basis-1/4 justify-self-start max-w-20 aspect-square md:shrink-0 bg-cover bg-no-repeat bg-center bg-profile"
+          className="profile-animation transition duration-150 ease-in-out w-9 basis-1/2 md:basis-1/4 justify-self-start max-w-20 aspect-square md:shrink-0 bg-cover bg-no-repeat bg-center bg-profile"
         ></div>
         <div data-aos="fade-left" className="basis-full md:basis-2/3">
           <h1 className="text-teal-900 text-2xl md:text-4xl font-bold subpixel-antialiased pb-5">
