14f777c442983ac4083a5e68b2f661ec728e2093
tanmui1992 <tanmui1992@gmail.com>
2023-01-21 23:48:19 +0700

    Feat: update profile

--- src/containers/Home/ProfilePhoto.jsx
+++ src/containers/Home/ProfilePhoto.jsx
@@ -4,8 +4,10 @@ function ProfilePhoto() {
   return (
     <div
       data-aos="fade-right"
-      className="profile-animation transition duration-150 ease-in-out w-9 basis-1/2 md:basis-1/4 justify-self-start max-w-20 aspect-square bg-cover bg-no-repeat bg-center bg-profile"
-    ></div>
+      className="profile-animation transition duration-150 ease-in-out w-9 basis-1/2 md:basis-1/4 justify-self-start max-w-20 aspect-square bg-cover bg-no-repeat bg-center"
+    >
+      hhahahaha
+    </div>
   );
 }
 
