0caf78cf64a80a8d24399c386ffc80a4631db624
tanmui1992 <tanmui1992@gmail.com>
2023-01-21 22:56:50 +0700

    feat: separated component

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -3,6 +3,7 @@ import Social from "./Social";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import "./Home.css";
+import ProfilePhoto from "./ProfilePhoto";
 
 function Home() {
   useEffect(() => {
@@ -16,10 +17,11 @@ function Home() {
         // className="container mt-8 grid-cols-3   bg-white-500"
       >
         <Social />
-        <div
+        {/* <div
           data-aos="fade-right"
           className="profile-animation transition duration-150 ease-in-out w-9 basis-1/2 md:basis-1/4 justify-self-start max-w-20 aspect-square md:shrink-0 bg-cover bg-no-repeat bg-center bg-profile"
-        ></div>
+        ></div> */}
+        <ProfilePhoto />
         <div data-aos="fade-left" className="basis-full md:basis-2/3">
           <h1 className="text-teal-900 text-2xl md:text-4xl font-bold subpixel-antialiased pb-5">
             Melody Le
---
+++ src/containers/Home/ProfilePhoto.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ProfilePhoto() {
+  return (
+    <div
+      data-aos="fade-right"
+      className="profile-animation transition duration-150 ease-in-out w-9 basis-1/2 md:basis-1/4 justify-self-start max-w-20 aspect-square md:shrink-0 bg-cover bg-no-repeat bg-center bg-profile"
+    ></div>
+  );
+}
+
+export default ProfilePhoto;
